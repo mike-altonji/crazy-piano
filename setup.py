9a7d659c87038320659b0a392e2e8a692d3302cb
Mike Altonji <mikealtonji@gmail.com>
2020-08-23 22:58:36 -0400

    Restoring crazy_piano folder
    Restoring the crazy_piano folder, after realizing I needed to change the GIT repository name.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="crazy-piano",
-    version="0.0.2",
+    version="0.0.3",
     author="Michael Altonji",
     author_email="mikealtonji@gmail.com",
     description="A package for exploring the fundamentals of how we define music.",
